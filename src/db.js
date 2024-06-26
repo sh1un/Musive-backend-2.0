8ec72834c3bbe9cf3057c0d7d52ef2fc738000ab
sh1un <a0923183408@gmail.com>
2024-06-27 03:21:37 +0800

    chore: disallowing database SSL connections

--- src/db.js
+++ src/db.js
@@ -1,15 +1,15 @@
 import pg from "pg";
 import dotenv from "dotenv";
 import "dotenv/config";
-dotenv.config({ path: "../.env" });
+dotenv.config({
+  path: "../.env"
+});
 
 const Pool = pg.Pool;
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: false,
 });
 
 export default pool;
